047dfe5c55df9e1904f34abab74cc97f262e2544
Nikita Feoktistov <nikitos.feo12@gmail.com>
2023-02-10 19:01:22 +0700

    added some functions

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <cstring>
+
+//Информация по товарам. Каждая запись состоит из следующих полей:
+// название товара (20 символов), цена(?), количество(?), дата получения товара (день, месяц, год),
+// срок годности (количество дней). Написать функции сортировки по названиям товаров и по датам получения товара.
+struct Date{
+int day;
+int month;
+int year;
+};
+
+struct Product{
+	char name[20];
+	double price;
+	int quantity;
+	Date receiving_date;
+	int expiration_date;
+
+};
+
+void sortByNameProducts(Product* products,int n);
+void sortByReceivingProducts(Product* products);
+void inputCheck();
+
+int comparator(const void *p, const void *q)
+{
+	char* l = ((struct Product *)p)->name;
+	char* r = ((struct Product *)q)->name;
+	return strcmp(l,r);
+}
+
+
+void sortByNameProducts(Product *products,int n){
+	qsort(products,n, sizeof(Product),comparator);
+}
+
+
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
